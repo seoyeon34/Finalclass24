a9d8a3301301e91ad89e4e22950788452d36308d
seoriny022 <seorin2022@gmail.com>
2025-11-22 04:40:35 +0900

    code for Login

--- ictunes/src/manage/Login.java
+++ ictunes/src/manage/Login.java
@@ -1,11 +1,44 @@
 package manage;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+1
 public class Login {
 
-	System.out.println("로그인 하시겠습까? --- (Y/N): ");
-	public static void main(String[] args) {
-		
+    public static void main(String[] args) {
 
-	}
+        JFrame frame = new JFrame("로그인");
+        frame.setSize(300, 150);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
 
+        JLabel label = new JLabel("로그인 하시겠습니까? ");
+        label.setBounds(70, 10, 200, 25);
+        frame.add(label);
+
+        JButton yesButton = new JButton("Y");
+        yesButton.setBounds(50, 50, 80, 30);
+        frame.add(yesButton);
+
+        JButton noButton = new JButton("N");
+        noButton.setBounds(150, 50, 80, 30);
+        frame.add(noButton);
+
+        yesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(frame, "로그인을 진행합니다.");
+            }
+        });
+
+        noButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(frame, "로그인을 취소합니다.");
+            }
+        });
+
+        frame.setVisible(true);
+    }
 }
