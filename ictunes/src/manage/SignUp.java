6e91565624a58da775446f64d4948692f65d930c
seoriny022 <seorin2022@gmail.com>
2025-11-22 04:38:40 +0900

    code for IDPW

--- ictunes/src/manage/SignUp.java
+++ ictunes/src/manage/SignUp.java
@@ -1,6 +1,14 @@
 package manage;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
 public class SignUp {
+	
+	public String ID = "";
+	public String PW = "";
+	
 	public static void main(String[] args) {
 		
 	}
