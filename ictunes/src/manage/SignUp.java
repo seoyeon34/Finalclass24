4f4f89e88eab94d69a72034e6ecc88ef057db797
seoriny022 <seorin2022@gmail.com>
2025-11-22 15:55:24 +0900

    code for sign up

--- ictunes/src/manage/SignUp.java
+++ ictunes/src/manage/SignUp.java
@@ -7,31 +7,48 @@ import javax.swing.*;
 
 public class SignUp extends JFrame {
 
+    public static String ID = "";
+    public static String PW = "";
+
+    JTextField idField;
+    JTextField pwField;
+
     public SignUp() {
         setTitle("Sign Up");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        GridLayout grid = new GridLayout(5, 2);
+        GridLayout grid = new GridLayout(6, 2);
         grid.setVgap(5);
 
         Container c = getContentPane();
         c.setLayout(grid);
+
         c.add(new JLabel("이름"));
         c.add(new JTextField(""));
+
         c.add(new JLabel("전화번호"));
         c.add(new JTextField(""));
-        c.add(new JLabel("아이디"));
-        c.add(new JTextField(""));
-        c.add(new JLabel("패스워드"));
-        c.add(new JTextField(""));
 
-        JButton Button = new JButton("완료");
-        c.add(Button);
+        c.add(new JLabel("아이디"));
+        idField = new JTextField(""); 
+        c.add(idField);
+
+        c.add(new JLabel("패스워드"));
+        pwField = new JTextField(""); 
+        c.add(pwField);
+
+        JButton completeBtn = new JButton("회원가입 완료");
+        c.add(completeBtn);
         c.add(new JLabel(""));
 
-        Button.addActionListener(new MyActionListener());
+        JButton loginBtn = new JButton("로그인");
+        c.add(loginBtn);
+        c.add(new JLabel(""));
 
-        setSize(300, 200);
+        completeBtn.addActionListener(new MyActionListener());
+        loginBtn.addActionListener(new MyActionListener());
+
+        setSize(300, 250);
         setVisible(true);
     }
 
@@ -39,8 +56,21 @@ public class SignUp extends JFrame {
         @Override
         public void actionPerformed(ActionEvent e) {
             JButton b = (JButton) e.getSource();
-            if (b.getText().equals("완료")) {
-                JOptionPane.showMessageDialog(SignUp.this, "회원가입이 완료되었습니다!");
+
+            if (b.getText().equals("회원가입 완료")) {
+                ID = idField.getText();
+                PW = pwField.getText();
+
+                if (ID.equals("") || PW.equals("")) {
+                    JOptionPane.showMessageDialog(SignUp.this, "아이디와 패스워드를 입력하세요.");
+                    return;
+                }
+
+                JOptionPane.showMessageDialog(SignUp.this, "회원가입이 완료되었습니다.");
+            }
+
+            if (b.getText().equals("로그인")) {
+                new Login();
             }
         }
     }
