6e93f695e4540a82bca889577074b18411e43fe7
seoriny022 <seorin2022@gmail.com>
2025-11-23 17:36:14 +0900

    code to find password

--- ictunes/src/manage/ForgotPassword.java
+++ ictunes/src/manage/ForgotPassword.java
@@ -1,10 +1,51 @@
 package manage;
 
-public class ForgotPassword {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class ForgotPassword extends JFrame {
 
-	}
+    JTextField phoneField;
 
+    public ForgotPassword() {
+        setTitle("비밀번호 찾기");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        GridLayout grid = new GridLayout(3, 2);
+        grid.setVgap(5);
+
+        Container c = getContentPane();
+        c.setLayout(grid);
+
+        c.add(new JLabel("전화번호 입력"));
+        phoneField = new JTextField("");
+        c.add(phoneField);
+
+        JButton findBtn = new JButton("비밀번호 찾기");
+        c.add(findBtn);
+        c.add(new JLabel(""));
+
+        findBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String inputPhone = phoneField.getText();
+
+                if (inputPhone.equals("")) {
+                    JOptionPane.showMessageDialog(ForgotPassword.this, "전화번호를 입력하세요.");
+                    return;
+                }
+
+                if (inputPhone.equals(SignUp.PHONE)) {
+                    JOptionPane.showMessageDialog(ForgotPassword.this,
+                        "아이디: " + SignUp.ID + "\n비밀번호: " + SignUp.PW);
+                } else {
+                    JOptionPane.showMessageDialog(ForgotPassword.this, "일치하는 정보가 없습니다.");
+                }
+            }
+        });
+
+        setSize(300, 150);
+        setVisible(true);
+    }
 }
