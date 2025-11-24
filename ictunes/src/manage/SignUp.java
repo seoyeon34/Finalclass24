9bc8ebe1a543c1a5e9f49610f0fcc822c22b2f8b
seoriny022 <seorin2022@gmail.com>
2025-11-24 20:29:38 +0900

    Enhance SignUp GUI with input fields and buttons
    Added input fields for name, phone number, ID, and password in the SignUp GUI.

--- ictunes/src/manage/SignUp.java
+++ ictunes/src/manage/SignUp.java
@@ -15,6 +15,7 @@ public class SignUp extends JFrame {
     JTextField idField;
     JTextField pwField;
 
+    //GUI 설정
     public SignUp() {
         setTitle("Sign Up");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -25,30 +26,37 @@ public class SignUp extends JFrame {
         Container c = getContentPane();
         c.setLayout(grid);
 
+        //이름 입력 필드, 라벨 추가 
         c.add(new JLabel("이름"));
         nameField = new JTextField("");
         c.add(nameField);
 
+        //전화번호 입력 필드, 라벨 추가 
         c.add(new JLabel("전화번호"));
         phoneField = new JTextField("");
         c.add(phoneField);
 
+        //아이디 입력 필드, 라벨 추가 
         c.add(new JLabel("아이디"));
         idField = new JTextField("");
         c.add(idField);
 
+        //패스워드 입력 필드, 라벨 추가 
         c.add(new JLabel("패스워드"));
         pwField = new JTextField("");
         c.add(pwField);
 
+        //회원가입 완료 버튼 
         JButton completeBtn = new JButton("회원가입 완료");
         c.add(completeBtn);
         c.add(new JLabel(""));
 
+        //로그인 버튼
         JButton loginBtn = new JButton("로그인");
         c.add(loginBtn);
         c.add(new JLabel(""));
 
+        //Listener 연결
         completeBtn.addActionListener(new MyActionListener());
         loginBtn.addActionListener(new MyActionListener());
 
@@ -61,11 +69,13 @@ public class SignUp extends JFrame {
         public void actionPerformed(ActionEvent e) {
             JButton b = (JButton) e.getSource();
 
+            //회원가입 완료 버튼 눌럿을 때
             if (b.getText().equals("회원가입 완료")) {
                 ID = idField.getText();
                 PW = pwField.getText();
                 PHONE = phoneField.getText();
 
+                //정보 다 입력했는지 확인
                 if (ID.equals("") || PW.equals("") || PHONE.equals("")) {
                     JOptionPane.showMessageDialog(SignUp.this, "정보를 모두 입력하세요.");
                     return;
@@ -74,6 +84,7 @@ public class SignUp extends JFrame {
                 JOptionPane.showMessageDialog(SignUp.this, "회원가입이 완료되었습니다.");
             }
 
+            //로그인 버튼 누르면 로그인 창 띄우기
             if (b.getText().equals("로그인")) {
                 new Login();
             }
